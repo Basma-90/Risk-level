a9d48a2c13b00f4a1ef8a5750a10f36635bc238c
Basma-90 <basmasabry33333@gmail.com>
2024-09-26 18:18:41 +0300

    Add API endpoints for retrieving and saving PHAs
    This commit adds new API endpoints for retrieving and saving PHAs (Potentially Hazardous Asteroids). The `/api/phas/:id` endpoint allows retrieving a specific PHA by its ID, while the `/api/phas` endpoint retrieves all PHAs. Additionally, the `/api/phas` endpoint now supports saving PHAs by sending a POST request with a start date and end date.
    
    The implementation includes necessary dependencies such as Express, body-parser, cors, and mongoose. It also includes the required models and controllers for handling PHAs.
    
    This commit follows the established convention for commit messages in the repository.

---
+++ src/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const PHA = require('./models/risk.model'); 
+const dotenv = require('dotenv');
+dotenv.config();
+const { getPHAs, savePHAs } = require('./controllers/risk.controllers');
+const app = express();
+const dbConnection= require('./config/db.config');
+dbConnection();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/api/phas/:id', async (req, res) => {
+    try {
+        const pha = await PHA.findById(req.params.id);
+        if (!pha) return res.status(404).json({ message: 'PHA not found' });
+        res.json(pha);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+app.post('/api/phas', async (req, res) => {
+    const startDate= "2000-01-01";
+    const endDate= "2000-01-02";
+    try {
+        await savePHAs(startDate, endDate);
+        res.json({ message: 'PHAs saved successfully.' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Failed to save PHAs.' });
+    }
+});
+
+app.get('/api/phas', async (req, res) => {
+    try {
+        const phas = await PHA.find();
+        res.json(phas);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
